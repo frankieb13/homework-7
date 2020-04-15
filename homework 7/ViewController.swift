601377b80d60d5daea13c8e00b45380f9802f771
Frankie Bonura <f.bonura@setonhill.edu>
2020-04-15 11:14:43 -0400

    Initial Commit

---
+++ homework 7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework 7
+//
+//  Created by Frank Bonura on 4/15/20.
+//  Copyright © 2020 Frank Bonura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ homework 7Tests/homework_7Tests.swift
@@ -0,0 +1,34 @@
+//
+//  homework_7Tests.swift
+//  homework 7Tests
+//
+//  Created by Frank Bonura on 4/15/20.
+//  Copyright © 2020 Frank Bonura. All rights reserved.
+//
+
+import XCTest
+@testable import homework_7
+
+class homework_7Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
